2935981a2f022f362bf5907900e7c340202def36
Patrick Swieskowski <pat@swieskowski.net>
2009-08-02 13:49:28 -0500

    no longer recommend already watched repos

--- popularity.py
+++ popularity.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python
-"""naive recommender based on popularity"""
+"""
+less naive recommender based on popularity
 
+doesn't recommend repos a user is already watching
+"""
+
+users = 56554
 repos = 123344
-counts = [ (0, None) ]*(repos+1)
+
+user_watches = []
+for i in range(users+1):
+    user_watches.append([])
+repo_watch_count = [ (0, None) ] * (repos+1)
+
+
+maxuser = 0
 
 for l in open("data/data.txt"):
     user, repo = [ int(x) for x in l.split(":") ]
-    counts[repo] = ( counts[repo][0]+1, repo )
+    user_watches[user].append(repo)
+    repo_watch_count[repo] = ( repo_watch_count[repo][0]+1, repo )
 
-counts.sort(key = lambda x: x[0], reverse = True)
-popular = ",".join([ str(x[1]) for x in counts[:10] ])
+repo_watch_count.sort(key = lambda x: x[0], reverse = True)
+popular_repos = [ x[1] for x in repo_watch_count[:1000] ]
 
-r = open("results.txt", "w")
+results = open("results.txt", "w")
 for l in open("data/test.txt"):
-    r.write(l.strip()+":"+popular+"\n")
+    u = int(l)
+    recs = []
+    for r in popular_repos:
+        if r not in user_watches[u]:
+            recs.append(r)
+        if len(recs) >= 10:
+            break
+    if len(recs) != 10:
+        print "Ran out of popular repos for user %d (%d recs)!" % (u, len(recs))
+    results.write("%d:%s\n" % (u, ",".join([str(x) for x in recs])))
